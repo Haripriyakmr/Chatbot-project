1135244b01ec6c1161671df061b68efcde4f2d41
Haripriya <138318492+Haripriyakmr@users.noreply.github.com>
2023-07-12 18:41:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Chatbox from './Components/Chatbox';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function App(props) {
+  return(
+    <div className='App'>
+    <Chatbox />
+    
+    </div>
+
+  )
+ 
+}
+
+export default App;
